dbeeb0f643942143d82420688645e6f84277ee40
venkat <venkatkoppula137@gmail.com>
2024-06-29 20:30:14 +0530

    changes-in-vite_config_file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my_portfolio/"
+  base: "/my_portfolio/",
+  build: { chunkSizeWarningLimit: 1600,}
 })
